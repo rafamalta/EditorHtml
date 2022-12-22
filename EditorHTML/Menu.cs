dddbacb8ad0dd692816902fb90ec1b9fff338409
Rafael Malta <rafa_rsbr@hotmail.com>
2022-12-22 10:41:40 -0300

    Desenhando a tela

--- EditorHTML/Menu.cs
+++ EditorHTML/Menu.cs
@@ -6,11 +6,56 @@
         {
             Console.Clear();
             // altera a cor do fundo
-            Console.BackgroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Blue;
             // altera a cor da fonte
             Console.ForegroundColor = ConsoleColor.Black;
 
             Console.WriteLine("Testando as cores");
+
+            CriarLinhaSuperior();
+            CriarColunas();
+            CriarLinhaInferior();
+        }
+
+        public static void CriarLinhaSuperior()
+        {
+            Console.Write("+");
+
+            for (int i = 0; i <= 30; i++)
+            {
+                Console.Write("-");
+            }
+
+            Console.Write("+");
+            Console.WriteLine();
+        }
+
+        public static void CriarColunas()
+        {
+            for (int linhas = 0; linhas <= 10; linhas++)
+            {
+                Console.Write("|");
+                for (int i = 0; i <= 30; i++)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.Write("|");
+                Console.WriteLine();
+            }
+
+            Console.Write("+");
+        }
+
+        public static void CriarLinhaInferior()
+        {
+            for (int i = 0; i <= 30; i++)
+            {
+                Console.Write("-");
+            }
+
+            Console.Write("+");
+            Console.WriteLine();
         }
     }
 }
