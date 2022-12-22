d1a016c89988d1ed33b63fe9f7f9c1171a6e0045
Rafael Malta <rafa_rsbr@hotmail.com>
2022-12-22 09:42:51 -0300

    Iniciando o projeto

---
+++ EditorHTML/Menu.cs
@@ -0,0 +1,16 @@
+﻿namespace EditorHTML
+{
+    static class Menu
+    {
+        public static void Exibir()
+        {
+            Console.Clear();
+            // altera a cor do fundo
+            Console.BackgroundColor = ConsoleColor.Green;
+            // altera a cor da fonte
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Testando as cores");
+        }
+    }
+}
---
+++ EditorHTML/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EditorHTML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.Exibir();
+
+            Console.ReadKey();
+        }
+    }
+}
