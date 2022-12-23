6640def44822a1eaec1f8ac12f921c280e9b1307
Rafael Malta <rafa_rsbr@hotmail.com>
2022-12-23 14:29:16 -0300

    Exibindo o Menu

--- EditorHTML/Menu.cs
+++ EditorHTML/Menu.cs
@@ -15,6 +15,9 @@
             CriarLinhaSuperior();
             CriarColunas();
             CriarLinhaInferior();
+            EscreverOpcoes();
+
+            var opcao = short.Parse(Console.ReadLine());
         }
 
         public static void CriarLinhaSuperior()
@@ -56,6 +59,25 @@
 
             Console.Write("+");
             Console.WriteLine();
+        }
+
+        public static void EscreverOpcoes()
+        {
+            // posiciona o cursor na coluna e linha informada no parâmetro
+            Console.SetCursorPosition(3,2);
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3, 3);
+            Console.WriteLine("============");
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("Selecione uma opção abaixo");
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("1 - Novo arquivo");
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("2 - Abrir");
+            Console.SetCursorPosition(3, 9);
+            Console.WriteLine("0 - Sair");
+            Console.SetCursorPosition(3, 10);
+            Console.Write("Opção: ");
         }
     }
 }
