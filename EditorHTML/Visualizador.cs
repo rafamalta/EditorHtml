8c7bbd9f94524ce336aacb5fb9ae3b7a88a0e11f
Rafael Malta <rafa_rsbr@hotmail.com>
2022-12-26 15:33:37 -0300

    Substituindo caracteres

--- EditorHTML/Visualizador.cs
+++ EditorHTML/Visualizador.cs
@@ -20,8 +20,29 @@ namespace EditorHTML
 
         public static void Substituir(string texto)
         {
-            var negrito = new Regex(@"<\s*negrito[^>]*>(.*?)<\s*/\s*negrito>");
-            Console.WriteLine(negrito);
+            var strong = new Regex(@"<\s*strong[^>]*>(.*?)<\s*/\s*strong>");
+            var words = texto.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (strong.IsMatch(words[i]))
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write(words[i].Substring(
+                        words[i].IndexOf('>') + 1,
+                        (
+                            words[i].LastIndexOf('<') - 1) -
+                            words[i].IndexOf('>'))
+                        );
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Write(words[i]);
+                    Console.Write(" ");
+                }
+            }
         }
     }
 }
